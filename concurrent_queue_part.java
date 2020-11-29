aef3494bbcca847bf13f17e7ebc1172fb58c6da8
chuck-hejun <57117086+chuck-hejun@users.noreply.github.com>
2020-11-29 12:44:30 -0500

    Add files via upload
    Reducing the locking overhead

---
+++ concurrent_queue_part.java
@@ -0,0 +1,19 @@
+//Reducing the locking overhead
+template<typename Data>
+class concurrent_queue_part
+{
+public:
+    void wait_and_pop(Data& popped_value)
+    {
+        boost::mutex::scoped_lock lock(the_mutex);
+        while(the_queue.empty())
+        {
+            the_condition_variable.wait(lock);
+        }
+        
+        popped_value=the_queue.front();
+        the_queue.pop();
+    }
+
+    // rest as before
+};
